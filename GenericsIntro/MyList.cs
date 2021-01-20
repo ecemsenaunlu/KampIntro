1b9c4e42cc03d69652e35621ce3d284d7e6fe554
ecem <ecemsenaunlu@gmail.com>
2021-01-20 23:50:32 +0300

    20.01.20

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor 
+        //ctor tab tab
+        public MyList()
+        {
+            items = new T[0];
+
+        }
+
+        public void Add(T item)
+        {
+            // tempArray geçici dizi newlediğimizde kaybolacak olan dizi elemanları tutması için 
+
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            //PascalCase            //camelCase
+            // case sensitive küçük harf büyük harf duyarlı
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            // int, double,bool...değer tip
+            // diziler,class,abstract class,interface ... referans tip
+            // ref out
+         
+        }
+    }
+}
